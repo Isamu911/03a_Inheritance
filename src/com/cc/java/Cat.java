d2f5897ebc2ab355f9877918379ee2a01b77bb95
Isamu911 <mwasielewski@gmx.de>
2021-12-06 09:06:46 +0100

    2

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -9,11 +9,12 @@ public class Cat {
 
     private int counter;
 
-    public Cat(String name, String furColor, int age, boolean isFemale) {
+    public Cat(String name, String furColor, int age) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
-        this.isFemale = isFemale;
+       
+        
     }
 
     public String getStringAttributes(String flag){
@@ -28,16 +29,9 @@ public class Cat {
     }
 
     public String getAge(){
-        return checkCompliance();
-    }
-
-    private String checkCompliance() {
-        if (isFemale) {
-            return checkEscalationLevel();
-        } else {
-            return Integer.toString(age); 
+  return checkEscalationLevel();
         }   
-    }
+    
 
     private String checkEscalationLevel(){
 
